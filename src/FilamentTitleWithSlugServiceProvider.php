83dc6c955da88a7d5158f2e3a71042171271ea68
Balazs Ercsey <laze@laze.hu>
2023-11-17 11:47:48 +0100

    Update FilamentTitleWithSlugServiceProvider.php

--- src/FilamentTitleWithSlugServiceProvider.php
+++ src/FilamentTitleWithSlugServiceProvider.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace Camya\Filament;
+namespace Elementary\Filament;
 
-use Filament\PluginServiceProvider;
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentTitleWithSlugServiceProvider extends PluginServiceProvider
+class FilamentTitleWithSlugServiceProvider extends PackageServiceProvider
 {
+    public static string $name = 'filament-title-with-slug';
+
     public function configurePackage(Package $package): void
     {
         $package
@@ -21,5 +25,12 @@ class FilamentTitleWithSlugServiceProvider extends PluginServiceProvider
         return [
             'filament-title-with-slug-styles' => __DIR__.'/../resources/dist/filament-title-with-slug.css',
         ];
+    }
+
+    public function packageBooted(): void
+    {
+        FilamentAsset::register([
+            Css::make('filament-title-with-slug-styles', __DIR__ . '/../resources/dist/filament-title-with-slug.css')->loadedOnRequest(),
+        ], 'awcodes/headings');
     }
 }
